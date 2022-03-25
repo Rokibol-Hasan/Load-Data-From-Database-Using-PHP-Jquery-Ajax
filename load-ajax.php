3a87dc2930a00e9da4ae538465e4ff86353d2d71
Rokibol-Hasan <rokibul.techdynobd@gmail.com>
2022-03-25 23:41:55 +0600

    Table Updated

--- load-ajax.php
+++ load-ajax.php
@@ -1,30 +1,36 @@
 <?php
-
+$x = 1;
 $conn = mysqli_connect("localhost", "root", "", "db_jquery") or die("Connection Failed");
 $sql = "SELECT * FROM tbl_user";
-$result = mysqli_query($conn, $sql) or die("sql failed");
-$output = "";
-if (mysqli_num_rows($result) > 0) {
-    $output = '<table>
-    <tr>
-        <th>
-            ID
-        </th>
-        <th>
-            Name
-        </th>
-    </tr>';
-    while ($row = mysqli_fetch_assoc($result)) {
-        $output .= "
+$result = mysqli_query($conn, $sql) or die("sql failed"); ?>
+<table class="table table-striped">
+    <thead>
         <tr>
-        <td>{$row['userid']}</td>
-        <td>{$row['username']}</td>
+            <th scope="col">
+                ID
+            </th>
+            <th scope="col">
+                First Name
+            </th>
+            <th scope="col">
+                Last Name
+            </th>
         </tr>
-        ";
+    </thead>
+    <?php if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) { ?>
+            <tbody>
+                <tr>
+                    <td> <?php echo $x;
+                            $x++; ?> </td>
+                    <td><?php echo $row['firstName'] ?></td>
+                    <td><?php echo $row['lastName'] ?></td>
+                </tr>
+            </tbody>
+    <?php }
+        mysqli_close($conn);
+    } else {
+        echo "Record Not Found";
     }
-    $output .= "</table>";
-    mysqli_close($conn);
-    echo $output;
-} else {
-    echo "Record Not Found";
-}
+    ?>
+</table>
